f51b4c2b60710a3da37587a764aeefa4920a24e9
Ralf Ebert <ralf.ebert@gmail.com>
2008-09-27 21:08:20 +0200

    prototyping deactivated for the moment

--- plugins/com.swtxml/src/com/swtxml/swt/CompatibilityNamespaceResolver.java
+++ plugins/com.swtxml/src/com/swtxml/swt/CompatibilityNamespaceResolver.java
@@ -1,11 +1,6 @@
 package com.swtxml.swt;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 import com.swtxml.definition.INamespaceDefinition;
-import com.swtxml.definition.ITagDefinition;
 import com.swtxml.parser.INamespaceResolver;
 import com.swtxml.swt.metadata.SwtNamespace;
 
@@ -13,28 +8,10 @@ import com.swtxml.swt.metadata.SwtNamespace;
 public class CompatibilityNamespaceResolver implements INamespaceResolver {
 
 	private static final SwtNamespace SWT_NAMESPACE = new SwtNamespace();
-	private static Map<String, FakeTagDefinition> prototypeTags = new HashMap<String, FakeTagDefinition>();
-
-	static {
-		prototypeTags.put("list", new FakeTagDefinition("list"));
-		prototypeTags.put("table", new FakeTagDefinition("table"));
-		prototypeTags.put("row", new FakeTagDefinition("row"));
-	}
 
 	public INamespaceDefinition resolveNamespace(String uri) {
 		if (uri.equals("class://com.swtxml.swt.SwtWidgetTagLibrary")) {
 			return SWT_NAMESPACE;
-		} else if (uri.equals("class://com.swtxml.swt.PrototypeTagLibrary")) {
-			return new INamespaceDefinition() {
-
-				public ITagDefinition getTag(String name) {
-					return prototypeTags.get(name);
-				}
-
-				public Set<String> getTagNames() {
-					return prototypeTags.keySet();
-				}
-			};
 		}
 		return null;
 	}
