7f7fad693f029ecb62921450c5a9dfc0cc35d679
Ralf Ebert <ralf.ebert@gmail.com>
2008-09-28 12:08:38 +0200

    superclasses are considered when injecting by id

--- plugins/com.swtxml/src/com/swtxml/swt/byid/ByIdInjector.java
+++ plugins/com.swtxml/src/com/swtxml/swt/byid/ByIdInjector.java
@@ -8,24 +8,28 @@ import com.swtxml.util.parser.ParseException;
 public class ByIdInjector {
 
 	public void inject(Object controller, IIdResolver ids) {
-		// TODO: consider superclass methods
-		for (Field field : controller.getClass().getDeclaredFields()) {
-			if (field.isAnnotationPresent(ById.class)) {
-				try {
-					Object value = ids.getById(field.getName(), field.getType());
-					if (value == null) {
-						throw new ParseException("No element with id " + field.getName()
-								+ " found for injecting @ById");
+		// TODO: use reflector api for finding these methods
+		Class<? extends Object> clazz = controller.getClass();
+		do {
+			for (Field field : clazz.getDeclaredFields()) {
+				if (field.isAnnotationPresent(ById.class)) {
+					try {
+						Object value = ids.getById(field.getName(), field.getType());
+						if (value == null) {
+							throw new ParseException("No element with id " + field.getName()
+									+ " found for injecting @ById");
+						}
+						boolean oldAccess = field.isAccessible();
+						field.setAccessible(true);
+						field.set(controller, value);
+						field.setAccessible(oldAccess);
+					} catch (Exception e) {
+						throw new ParseException(e);
 					}
-					boolean oldAccess = field.isAccessible();
-					field.setAccessible(true);
-					field.set(controller, value);
-					field.setAccessible(oldAccess);
-				} catch (Exception e) {
-					throw new ParseException(e);
 				}
 			}
-		}
+			clazz = clazz.getSuperclass();
+		} while (clazz != null && !Object.class.equals(clazz));
 	}
 
 }
